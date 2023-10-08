0590522767fa9c504b115f09ca252318450a0127
houss <houssam2001mchmch@gmail.com>
2023-10-08 01:10:52 +0100

    space

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,121 @@
+function Header2 ( ){
+    return(
+      <div className="w-[1440px] h-[7408px] relative bg-gradient-to-b from-slate-950 via-slate-950 to-slate-950">
+      <img className="w-[1441px] h-[848px] left-[-1px] top-[3894px] absolute" src="https://via.placeholder.com/1441x848" />
+      <div className="w-[1441px] h-[848px] left-[-1px] top-[3894px] absolute bg-gradient-to-b from-white via-white to-white" />
+      <div className="w-[92px] h-[22px] left-[765px] top-[29px] absolute text-white text-xl font-normal font-['Jockey One']">PLANETS</div>
+      <div className="w-[121px] h-[43px] left-[921px] top-[30px] absolute text-white text-xl font-normal font-['Jockey One']">SPACE MISSIONS</div>
+      <div className="w-[76px] h-[22px] left-[1123px] top-[29px] absolute text-white text-xl font-normal font-['Jockey One']">HELP</div>
+      <div className="w-[60px] h-[22px] left-[615px] top-[29px] absolute text-white text-xl font-normal font-['Jockey One']">HOME</div>
+      <div className="w-[70px] h-[22px] left-[1289px] top-[29px] absolute text-white text-xl font-normal font-['Jockey One']">ABOUT</div>
+      <div className="w-[716px] h-[716px] left-[840px] top-[113px] absolute">
+          <div className="w-[716px] h-[716px] left-0 top-0 absolute bg-stone-500 rounded-full blur-[51px]" />
+          <img className="w-[716px] h-[716px] left-0 top-0 absolute" src="https://via.placeholder.com/716x716" />
+      </div>
+      <div className="w-[181px] h-[55px] left-[75px] top-[45px] absolute" />
+      <img className="w-[1379px] h-[419px] left-[26px] top-[2801px] absolute" src="https://via.placeholder.com/1379x419" />
+      <div className="w-[172px] h-[38px] left-[355px] top-[211px] absolute text-white text-[28.52px] font-normal font-['Jockey One'] tracking-[17.11px]">PLANET</div>
+      <div className="w-[491px] h-[118px] left-[215px] top-[285px] absolute text-white text-[88.56px] font-normal font-['Krona One'] tracking-[37.20px]">MARS</div>
+      <div className="w-[297px] h-[88px] left-[24px] top-[22px] absolute text-white text-3xl font-normal font-['Krona One'] tracking-[12.60px]">SPACE<br/>INVADERS</div>
+      <div className="w-[617px] h-[51px] left-[149px] top-[940px] absolute text-neutral-400 text-[38.01px] font-normal font-['Krona One']">Our Services</div>
+      <div className="w-[235px] h-[54px] left-[467px] top-[634px] absolute">
+          <div className="w-[235px] h-[54px] left-0 top-0 absolute bg-zinc-300 bg-opacity-30 rounded-[41px] border border-white" />
+          <div className="w-[174px] h-7 left-[60px] top-[13px] absolute text-white text-xl font-normal font-['Jockey One']">DISCOVER MORE</div>
+      </div>
+      <div className="w-[235px] h-[54px] left-[172px] top-[634px] absolute">
+          <div className="w-[235px] h-[54px] left-0 top-0 absolute bg-zinc-300 bg-opacity-30 rounded-[41px] border border-white" />
+          <div className="w-24 left-[69px] top-[13px] absolute text-white text-xl font-normal font-['Jockey One']">TRY IT NOW</div>
+      </div>
+      <div className="w-[1440px] h-[1218px] left-0 top-[811px] absolute bg-zinc-300 bg-opacity-5 rounded-[60px] shadow" />
+      <div className="w-[241px] h-[54px] left-[597px] top-[1905px] absolute">
+          <div className="w-[241px] h-[54px] left-0 top-0 absolute bg-zinc-300 bg-opacity-40 rounded-[41px] border border-white" />
+          <div className="w-[109.72px] left-[65.02px] top-[13px] absolute text-white text-xl font-normal font-['Jockey One']">EXPLORE MORE</div>
+      </div>
+      <div className="w-[241px] h-[54px] left-[599px] top-[4637px] absolute">
+          <div className="w-[241px] h-[54px] left-0 top-0 absolute bg-zinc-300 bg-opacity-40 rounded-[41px] border border-white" />
+          <div className="w-[109.72px] left-[65.02px] top-[13px] absolute text-white text-xl font-normal font-['Jockey One']">EXPLORE MORE</div>
+      </div>
+      <div className="w-[558px] h-[39px] left-[444px] top-[1719px] absolute text-neutral-400 text-[29.06px] font-normal font-['Krona One']">Join Our Cosmic Adventure!</div>
+      <div className="w-[602px] h-[55px] left-[163px] top-[440px] absolute text-amber-100 text-opacity-90 text-[28px] font-normal font-['Krona One']">EXPLORE OUR SPACE MISSIONS</div>
+      <img className="w-[364px] h-[544px] left-[149px] top-[1029px] absolute" src="https://via.placeholder.com/364x544" />
+      <img className="w-[360px] h-[544px] left-[543px] top-[1029px] absolute" src="https://via.placeholder.com/360x544" />
+      <div className="w-[318px] h-[67px] left-[149px] top-[1586px] absolute text-white text-xl font-normal font-['Lao Muang Khong']">Immersive Space Exploration Experience</div>
+      <div className="w-[311px] h-[65px] left-[543px] top-[1586px] absolute text-white text-xl font-normal font-['Lao Muang Khong']">Educational Content and Learning Modules</div>
+      <div className="w-[290px] h-16 left-[933px] top-[1586px] absolute text-white text-xl font-normal font-['Lao Muang Khong']">Customized VR Development and Integration</div>
+      <div className="w-[631px] h-[76px] left-[147px] top-[495px] absolute text-center text-white text-opacity-80 text-[23px] font-normal font-['Lao Muang Khong']">Embark on educational space missions like never before.<br/>Blast off into an educational adventure, learning about space in an immersive VR experience.</div>
+      <div className="w-[891px] h-[38px] left-[274px] top-[1797px] absolute text-center text-violet-300 text-[23px] font-normal font-['Lao Muang Khong']">Our Team of Experts is Committed to Crafting an Unforgettable Mars Exploration Journey!</div>
+      <div className="w-[341px] h-[63px] left-[291px] top-[2643px] absolute text-center"><span >30 Days<br/></span><span > of medical and Physiical preparation</span></div>
+      <div className="w-[341px] h-[83px] left-[836px] top-[2766px] absolute text-center"><span >10 Days<br/></span><span > preparation of document and agreement</span></div>
+      <div className="w-[341px] h-[74px] left-[291px] top-[2912px] absolute text-center"><span >10 Days</span><span > <br/>transfer to a moon</span></div>
+      <div className="w-[341px] h-[78px] left-[836px] top-[3072px] absolute text-center"><span >2 Days<br/></span><span > transfer to a Axiom spaceship</span></div>
+      <div className="w-[341px] h-[89px] left-[289px] top-[3189px] absolute text-center"><span >160 Days<br/></span><span > spaceship flight to mars </span></div>
+      <div className="w-[341px] h-[89px] left-[828px] top-[3312px] absolute text-center"><span >5 Days<br/></span><span > Adaptation and Medical Examination </span></div>
+      <img className="w-[362px] h-[362px] left-[539px] top-[3341px] absolute shadow" src="https://via.placeholder.com/362x362" />
+      <img className="w-[362px] h-[362px] left-[535px] top-[3343px] absolute" src="https://via.placeholder.com/362x362" />
+      <div className="w-[395px] h-[39px] left-[522px] top-[2078px] absolute text-neutral-50 text-[29.06px] font-normal font-['Krona One']">YOUR WAY TO MARS</div>
+      <div className="w-[789px] h-[39px] left-[325px] top-[3938px] absolute text-neutral-50 text-[29.06px] font-normal font-['Krona One']">DISCOVER THE THRILL OF SPACE TRAVEL</div>
+      <div className="w-[1316px] h-[95px] left-[67px] top-[3721px] absolute text-center text-neutral-50 text-[29.06px] font-normal font-['Krona One']">UTILIZING SPACE INVADERS, ACHIEVING ALL OF THIS TAKES JUST A FEW CLICKS, AND WITHIN MERE SECONDS, YOU'LL ALREADY ARRIVE AT YOUR DESTINATION.</div>
+      <img className="w-[317px] h-[178px] left-[1197px] top-[2907px] absolute origin-top-left rotate-180" src="https://via.placeholder.com/317x178" />
+      <div className="w-[358.44px] h-[390px] left-[131px] top-[4096px] absolute">
+          <img className="w-[358.44px] h-[390px] left-[358.44px] top-0 absolute origin-top-left rotate-180" src="https://via.placeholder.com/358x390" />
+          <div className="w-[358.44px] h-[390px] left-0 top-0 absolute bg-gradient-to-b from-black to-black" />
+      </div>
+      <div className="w-[358.44px] h-[390px] left-[540.28px] top-[4096px] absolute">
+          <img className="w-[358.44px] h-[390px] left-[358.44px] top-0 absolute origin-top-left rotate-180" src="https://via.placeholder.com/358x390" />
+          <div className="w-[358.44px] h-[390px] left-0 top-0 absolute bg-gradient-to-b from-black to-black" />
+      </div>
+      <div className="w-[358.44px] h-[390px] left-[949.56px] top-[4096px] absolute">
+          <img className="w-[358.44px] h-[390px] left-[358.44px] top-0 absolute origin-top-left rotate-180" src="https://via.placeholder.com/358x390" />
+          <div className="w-[358.44px] h-[390px] left-0 top-0 absolute bg-gradient-to-b from-black to-black" />
+      </div>
+      <div className="origin-top-left rotate-[-63.60deg] w-[418.94px] h-[418.94px] left-[437px] top-[2512.27px] absolute">
+          <div className="w-[418.94px] h-[418.94px] left-0 top-0 absolute origin-top-left rotate-[-63.60deg] bg-zinc-300 rounded-full" />
+          <img className="w-[1072.60px] h-[805.08px] left-[-304.18px] top-[-0.23px] absolute origin-top-left rotate-[-63.60deg]" src="https://via.placeholder.com/1073x805" />
+      </div>
+      <div className="origin-top-left rotate-[-63.60deg] w-[418.94px] h-[418.94px] left-[437px] top-[2512.27px] absolute">
+          <div className="w-[418.94px] h-[418.94px] left-0 top-0 absolute origin-top-left rotate-[-63.60deg] bg-zinc-300 rounded-full" />
+          <img className="w-[1072.60px] h-[805.08px] left-[-304.18px] top-[-0.23px] absolute origin-top-left rotate-[-63.60deg]" src="https://via.placeholder.com/1073x805" />
+      </div>
+      <div className="w-[899px] left-[270px] top-[4534px] absolute text-center text-white text-2xl font-normal font-['Lao Muang Khong']">Panoramic Views: Enjoy breathtaking views of the cosmos through large, specially designed windows, offering a glimpse into the wonders of space.</div>
+      <div className="w-[441px] left-[522px] top-[7229px] absolute"><span >©</span><span > 2023 Space Invaders . All Rights Reserved.</span></div>
+      <div className="w-[219px] h-[54px] left-[642px] top-[6538px] absolute">
+          <div className="w-[219px] h-[54px] left-0 top-0 absolute bg-zinc-300 bg-opacity-40 rounded-[41px] border border-white" />
+          <div className="w-[92px] left-[63px] top-[13px] absolute text-white text-xl font-normal font-['Jockey One']">LEARN MORE</div>
+      </div>
+      <div className="w-[443px] h-[39px] left-[498px] top-[5946px] absolute text-neutral-50 text-[29.06px] font-normal font-['Krona One']">HOW RELIABLE WE ARE</div>
+      <div className="w-[1221px] h-[448.97px] left-[131px] top-[6061.03px] absolute">
+          <div className="w-[289.45px] h-[448.97px] left-[931.55px] top-0 absolute">
+              <div className="w-[289.45px] h-[443.49px] left-0 top-0 absolute bg-gradient-to-b from-zinc-300 to-zinc-300 rounded-[38px]" />
+              <div className="w-[230.30px] h-[60.88px] left-[39.45px] top-[114.97px] absolute text-neutral-50 text-[29.06px] font-normal font-['Krona One']">FEED BACK</div>
+              <div className="w-[245.86px] h-[244.61px] left-[22.84px] top-[204.35px] absolute text-center text-white text-xl font-normal font-['Lao Muang Khong']">Showcase a history of positive user feedback and reviews, highlighting the satisfaction and trust earned from previous users who have immersed themselves in your Mars exploration project</div>
+          </div>
+          <div className="w-[289.45px] h-[443.49px] left-0 top-0 absolute">
+              <div className="w-[289.45px] h-[443.49px] left-0 top-0 absolute">
+                  <div className="w-[289.45px] h-[443.49px] left-0 top-0 absolute bg-gradient-to-b from-zinc-300 to-zinc-300 rounded-[38px]" />
+                  <div className="w-[107.17px] h-[112.31px] left-[90.25px] top-[38.01px] absolute" />
+              </div>
+              <div className="w-[177px] h-[52px] left-[51px] top-[118.97px] absolute text-neutral-50 text-[29.06px] font-normal font-['Krona One']">Expertise</div>
+              <div className="w-[245.86px] h-[163.08px] left-[22px] top-[221.97px] absolute text-center text-white text-xl font-normal font-['Lao Muang Khong']">Leverage a team of professionals well-versed in space education and virtual reality technology, ensuring accurate and engaging information is presented to users.</div>
+          </div>
+          <div className="w-[304px] h-[443.49px] left-[614px] top-0 absolute">
+              <div className="w-[289.45px] h-[443.49px] left-[7.03px] top-0 absolute bg-gradient-to-b from-zinc-300 to-zinc-300 rounded-[38px]" />
+              <div className="w-[304px] left-0 top-[93.97px] absolute text-center text-neutral-50 text-[29.06px] font-normal font-['Krona One']">VR <br/>Integration</div>
+              <div className="w-[245.86px] h-[135.90px] left-[32px] top-[210.97px] absolute text-center text-white text-xl font-normal font-['Lao Muang Khong']">Utilize cutting-edge virtual reality technology and immersive design to ensure a seamless and captivating journey, enhancing user trust in the accuracy and realism of the experience</div>
+          </div>
+          <div className="w-[289.45px] h-[443.49px] left-[310.52px] top-0 absolute">
+              <div className="w-[289.45px] h-[443.49px] left-0 top-0 absolute bg-gradient-to-b from-zinc-300 to-zinc-300 rounded-[38px]" />
+              <div className="w-[257px] h-[70px] left-[21.48px] top-[100.97px] absolute text-center text-neutral-50 text-[29.06px] font-normal font-['Krona One']">Researched Content</div>
+              <div className="w-[254.16px] h-[197.87px] left-[16.48px] top-[210.97px] absolute text-center text-white text-xl font-normal font-['Lao Muang Khong']">Provide content that is meticulously researched, drawing from reputable sources and verified scientific data to offer an authentic and reliable Mars exploration experience</div>
+          </div>
+      </div>
+      <div className="w-[987px] h-[104px] left-[237px] top-[4801px] absolute">
+          <div className="w-[401.89px] h-[27.41px] left-[292.56px] top-0 absolute text-neutral-50 text-[29.06px] font-normal font-['Krona One']">DREAM COMES TRUE</div>
+          <div className="w-[987px] h-[19.68px] left-0 top-[84.32px] absolute text-center"><span >Turn your dreams into reality</span><span > - secure your ticket and embark on the journey of a lifetime</span></div>
+      </div>
+      <img className="w-[357px] h-[544px] left-[933px] top-[1029px] absolute" src="https://via.placeholder.com/357x544" />
+      <img className="w-[987px] h-[955px] left-[237px] top-[4963px] absolute" src="https://via.placeholder.com/987x955" />
+      <img className="w-[1440px] h-[524px] left-0 top-[6660px] absolute" src="https://via.placeholder.com/1440x524" />
+  </div>
+    );
+}
+export default Header2;
